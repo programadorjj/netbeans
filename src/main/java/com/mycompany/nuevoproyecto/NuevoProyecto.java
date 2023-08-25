9d8513ccdc7039b248393f953b84b94c1d91c725
Joel <joelhinojozabermudez@gmail.com>
2023-08-25 17:10:00 +0100

    primera version

---
+++ src/main/java/com/mycompany/nuevoproyecto/NuevoProyecto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nuevoproyecto;
+
+/**
+ *
+ * @author Alumno
+ */
+public class NuevoProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("esto es un hola mundo desde NetBeans");
+    }
+}
